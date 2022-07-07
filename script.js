a9a0432220b1df89d67650c906cc53682b0cd955
Dany Knight <gnuwaves@gmail.com>
2022-07-07 19:19:18 -0400

    add password validation

---
+++ script.js
@@ -0,0 +1,84 @@
+var myInput = document.getElementById("password");
+var letter = document.getElementById("letter");
+var capital = document.getElementById("capital");
+var number = document.getElementById("number");
+var length = document.getElementById("length");
+
+// When the user clicks on the password field, show the message box
+myInput.onfocus = function() {
+  document.getElementById("message").style.display = "block";
+}
+
+// When the user clicks outside of the password field, hide the message box
+myInput.onblur = function() {
+  document.getElementById("message").style.display = "none";
+}
+
+// When the user starts to type something inside the password field
+myInput.onkeyup = function() {
+  // Validate lowercase letters
+  var lowerCaseLetters = /[a-z]/g;
+  if(myInput.value.match(lowerCaseLetters)) {
+    letter.classList.remove("invalid");
+    letter.classList.add("valid");
+  } else {
+    letter.classList.remove("valid");
+    letter.classList.add("invalid");
+}
+
+  // Validate capital letters
+  var upperCaseLetters = /[A-Z]/g;
+  if(myInput.value.match(upperCaseLetters)) {
+    capital.classList.remove("invalid");
+    capital.classList.add("valid");
+  } else {
+    capital.classList.remove("valid");
+    capital.classList.add("invalid");
+  }
+
+  // Validate numbers
+  var numbers = /[0-9]/g;
+  if(myInput.value.match(numbers)) {
+    number.classList.remove("invalid");
+    number.classList.add("valid");
+  } else {
+    number.classList.remove("valid");
+    number.classList.add("invalid");
+  }
+
+  // Validate length
+  if(myInput.value.length >= 8) {
+    length.classList.remove("invalid");
+    length.classList.add("valid");
+  } else {
+    length.classList.remove("valid");
+    length.classList.add("invalid");
+  }
+}
+
+
+
+
+
+
+function checkPassword(){
+    let pass = document.getElementById("password").value;
+    let cfmpass = document.getElementById("confirm_pass").value;
+    console.log("Password:", pass,'\n',"Confirm Password:",cfmpass);
+    let message = document.getElementById("message2");
+    let submit = document.querySelector('[onsubmit]');
+
+  if(pass.length != 0){
+    if(pass == cfmpass){
+      submit.onsubmit = "true";
+      message.textContent = "Passwords match";
+      message.style.backgroundColor = "green";
+    } else{
+      message.textContent = "Passwords do not match";
+      message.style.backgroundColor = "red";
+    }
+  } else{
+    // alert("Password can't be empty!");
+    message.textContent = "";
+  }
+}
